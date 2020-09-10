48983d659e01975b5ca1a7f5c43033f25efe518c
jay311311 <jay311311@gmail.com>
2020-09-10 14:24:29 +0900

    first-commit

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import GlobalStyles from "Components/GlobalStyles"
+import Home from "./Home"
+
+
+
+ export default class App extends Component{
+  
+   render(){
+
+    return(
+      <>
+      <GlobalStyles/>
+      <Home/>
+    </>
+    )
+   }
+ }
---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,20 @@
+
+import {createGlobalStyle} from "styled-components"//createGlobalStyle에 styled-component를 사용 할거다
+import reset from "styled-reset" // styled-reset 을 사용하기 위해
+
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration : none;
+        color:inherit;
+    }
+    *{box-sizing:border-box;}
+    body{
+        font-family : sanserif;
+        font-size: 12px;
+        background-color:rgba(20,20,20,1);
+        color:white;
+        padding-top:50px;
+    }
+`;
+export default GlobalStyles;
